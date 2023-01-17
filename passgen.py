eb6e41de5625005215641760437f1dfa18bccbfa
Dan Coman <dancoman@Dans-MacBook-Pro.local>
2023-01-17 12:02:11 +0000

    Adding to GitHub with dummy txt file

---
+++ passgen.py
@@ -0,0 +1,37 @@
+import random
+
+word_list = []
+password = []
+
+def generate_words():
+    f = open("words.txt", "r")
+    for word in f:
+        if len(word) > 5 and len(word) < 12:
+            word_list.append(word.strip())
+
+generate_words()
+
+def word_picker():
+    num = random.randrange(0, 5)
+    return word_list[num]
+
+def random_number():
+    number = random.randrange(100, 1000)
+    return number
+
+def password_generator():
+    first_word = word_picker()
+    password.append(first_word.capitalize())
+    number = random_number()
+    password.append(str(number))
+    second_word = word_picker()
+    password.append(second_word)
+    print("".join(password))
+
+i = 0
+
+while i < 5:
+    password_generator()
+    password = []
+    i += 1
+
